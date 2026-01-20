d959db732714916bfe2862f77ea5550f3e83e439
Vyzdryk Mykhailo <wyzdrykm@gmail.com>
2026-01-20 12:37:50 +0200

    Single blog stores

--- patterns/custom-cta-section.php
+++ patterns/custom-cta-section.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Custom CTA Section
  * Slug: hyring/custom-cta-section
@@ -10,9 +11,13 @@
  * @since Hyring 1.0
  */
 
-$bg_image_url = esc_url( get_theme_file_uri( 'assets/images/start_hiring_bg.jpg' ) );
+$bg_image_url = esc_url(get_theme_file_uri('assets/images/start_hiring_bg.jpg'));
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-helooo
+<div style="padding:38px 10px !important;min-height:432px; gap:0px !important;width:100%;max-width:100%;background: url('<?php echo esc_url(get_theme_file_uri('assets/images/start_hiring_bg.jpg')); ?>') no-repeat bottom center;
+background-size: cover; display: flex; flex-direction:column; align-items:center;justify-content:flex-end;">
+    <a style="margin:0px !important" href="https://play.google.com/store/apps/details?id=ai.hyring.app"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/google_play.svg')); ?>" alt="App Store"></a>
+    <a style="margin:0px !important" href="https://apps.apple.com/us/app/hyring-ai/id6502471201"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/app_store.svg')); ?>" alt="App Store"></a>
+
 </div>
 <!-- /wp:group -->
